a35bc37c4f073fd059520a9d3900c68a902e8813
rezzubs <marten.roots@gmail.com>
2023-10-13 23:20:26 +0300

    catpuccin-gtk/backup

--- home.nix
+++ home.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  catppuccin-gtk = pkgs.catppuccin-gtk.override {
+    accents = [ "lavender" ];
+    size = "standard";
+    tweaks = [ "rimless" ];
+    variant = "mocha";
+  };
+in
 {
   nixpkgs.config.allowUnfree = true;
   
@@ -27,12 +35,24 @@
     libreoffice
     qdigidoc
 
+
+    dmenu
+    feh
+    dunst
+    xclip
+    pavucontrol
+    redshift
+
     haskell-language-server
+    haskellPackages.fourmolu
+    ghcid
     lua-language-server
     nodejs_20
     vscode-langservers-extracted
+  ] ++ [
+    catppuccin-gtk
   ];
-
+  
   services.syncthing.enable = true;
   
   home.sessionPath = [
@@ -65,6 +85,24 @@
     };
   };
 
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "FiraCode Nerd Font";
+      size = 15;
+    };
+    settings = {
+      tab_bar_style = "powerline";
+      tab_powerline_style = "round";
+      window_padding_width = 15;
+      linux_display_server = "x11";
+    };
+    extraConfig = ''
+      map ctrl+shift+t launch --cwd=current --type=tab 
+    '';
+    theme = "Catppuccin-Mocha";
+  };
+
   programs.starship.enable = true;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -80,6 +118,11 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    
+
+    ".config/gtk-4.0/assets".source = "${catppuccin-gtk}/share/themes/Catppuccin-Mocha-Standard-Lavender-dark/assets";
+    ".config/gtk-4.0/gtk.css".source = "${catppuccin-gtk}/share/themes/Catppuccin-Mocha-Standard-Lavender-dark/gtk-4.0/gtk.css";
+    ".config/gtk-4.0/gtk-dark.css".source = "${catppuccin-gtk}/share/themes/Catppuccin-Mocha-Standard-Lavender-dark/gtk-4.0/gtk-dark.css";
   };
 
   # You can also manage environment variables but you will have to manually
